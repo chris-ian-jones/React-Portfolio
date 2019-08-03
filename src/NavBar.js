0b35f52ce77ef77813231741426fcc4a86ae8b84
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-03 22:59:43 +0100

    WIP: intial setup commit

---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledPlaceholder = styled.p`
+    color: red;
+`
+
+function NavBar() {
+
+    return(    
+        <div>
+            <StyledPlaceholder>nav placeholder</StyledPlaceholder>
+        </div>
+    )
+}
+
+export default NavBar
